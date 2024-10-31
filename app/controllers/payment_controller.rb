a362f1eed019a121994654cf7f738f7f573e6d72
jenchan-938 <jchan5@uchicago.edu>
2024-10-31 03:36:57 +0000

    1036

--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -2,14 +2,7 @@
 class PaymentController < ApplicationController
 
   def payment
-  <form action ="/results">
-    <label for ="user_apr"> </label>
-    <input id ="user_apr" name="APR" placeholder= "E.g. 5.42"> 
-    <button>Calculate monthly payment</button>
-  </form>
-
-
-
-  render({ :template =>"layouts/payment"})
+    
+  render({ :template =>"layouts/payments"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get("/square/new", { :controller => "square", :action => "square"})
-  get("/square_root/new", { :controller =>"square_r", action => "square_r"})
+  #get("/square/new", { :controller => "square", :action => "square"})
+  #get("/square_root/new", { :controller =>"square_r", action => "square_r"})
   get("/payment/new", { :controller => "payment", :action => "payment"})
-  get("/random/new", { :controller => "random", :action => "random"})
+  get("/payment/results", { :controller => "payment_r", :action => "payment"})
+  #get("/random/new", { :controller => "random", :action => "random"})
 
 end
